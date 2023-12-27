fac9617c413be92e66c351bff06d29a2e7cb9dd9
yuangwei <yuang.wya@gmail.com>
2023-12-27 17:58:54 +0800

    feat: update

--- i18n.go
+++ i18n.go
@@ -1,12 +1,20 @@
 package i18next
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
-	"github.com/yuangwei/go-i18next/internal/load"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path"
+	"strings"
+	"text/template"
 )
 
 type Backend struct {
-	LoadPath    []string // from network or local
+	LoadPath    []string // from network or local, usage: https://example.com/locales/{{.Lng]}/locale.json
 	CrossDomain bool
 }
 
@@ -16,12 +24,9 @@ type CodeOptions struct {
 }
 
 type I18nOptions struct {
-	Lng         []string                     // example: ["en-US", "fr-FR"]
-	FallbackLng []string                     // example: ["en-US"]
+	Lng         []string                     // example: ["en-US", "fr-FR"] usage: /locale/{{.Lng}}/locale.json or https://example.com/locales/{{.Lng]}/locale.json
 	DefaultLng  string                       // example: "en-US"
-	NS          []string                     // example: ["dev", "prod"]
-	DefaultNs   string                       // example: "dev", , not require. usage: https://example.com/locales/{{.Ns}}/{{.Lng]}.json
-	Preload     bool                         // preload lng resource.
+	Ns          string                       // example: "dev", , not require. usage: https://example.com/locales/{{.Ns}}/{{.Lng]}.json
 	Backend     Backend                      // load lngs from file or network
 	Resources   map[string]map[string]string // load lngs from local
 	CodeOptions CodeOptions
@@ -29,38 +34,179 @@ type I18nOptions struct {
 
 type I18n struct {
 	langs map[string]string
+	opts  I18nOptions
 }
 
-func Init(options I18nOptions) I18n {
+func Init(options I18nOptions) (I18n, error) {
 	i18n := &I18n{}
-	if options.Resources != nil {
-		langs, err := load.LoadResourceInLocal(
-			options.DefaultLng,
-			options.FallbackLng,
-			options.Resources,
-			options.CodeOptions.LowerCaseLng,
-			options.CodeOptions.CleanCode,
-		)
-		if err != nil {
-			panic(any(err))
-		}
-		i18n.langs = langs
+	i18n.opts = options
+	langs, err := i18n.loadLangs(i18n.opts.DefaultLng)
+	if err != nil {
+		return I18n{}, err
 	}
-	return *i18n
+	i18n.langs = langs
+	return *i18n, nil
 }
 
 func (i *I18n) Exist(key string) bool {
-	// tmpl, err := template.New("test").Parse("{{.Count}} items are made of {{.Material}}")
+	if _, ok := i.langs[key]; ok {
+		return true
+	}
+	return false
 }
 
-func (i *I18n) T(key string, props ...string) (string, error) {
+func (i *I18n) T(key string, props interface{}) (string, error) {
 	if v, ok := i.langs[key]; ok {
+		tmpl, err := template.New("path").Parse(v)
+		if err != nil {
+			return "", err
+		}
+		err = tmpl.Execute(os.Stdout, props)
 		return v, nil
 
 	}
 	return "", errors.New("Not found")
 }
 
-func (i *I18n) ChangeLanguage(lang string) error {
+func (i *I18n) ChangeLanguage(lng string) error {
+	langs, err := i.loadLangs(lng)
+	if err != nil {
+		return err
+	}
+	i.langs = langs
+	return nil
+}
 
+func (i *I18n) loadLangs(lng string) (map[string]string, error) {
+	var resources map[string]map[string]string
+	backend := i.opts.Backend
+	codeOpts := i.opts.CodeOptions
+	lngs := i.opts.Lng
+	if !contains(lngs, lng) {
+		return nil, errors.New(fmt.Sprintf("%s is not in Lngs", lng))
+	}
+	if backend.LoadPath != nil {
+		res, err := getLangs(i.opts)
+		if err != nil {
+			return nil, err
+		}
+		resources = res
+	} else {
+		resources = i.opts.Resources
+	}
+
+	if resources == nil {
+		return nil, errors.New("resource is blank")
+	}
+	return loadResource(lng, resources, codeOpts)
+}
+
+func loadResource(lng string, resources map[string]map[string]string, codeOpt CodeOptions) (map[string]string, error) {
+	lang := transferLangCode(lng, codeOpt.LowerCaseLng, codeOpt.CleanCode)
+	res, ok := resources[lang]
+	if ok {
+		return res, nil
+	}
+	return nil, errors.New("resource not found")
+}
+
+func getLangs(opts I18nOptions) (map[string]map[string]string, error) {
+	var resources map[string]map[string]string
+	var op = struct {
+		Ns  string
+		Lng string
+	}{Ns: opts.Ns, Lng: opts.DefaultLng}
+	p := opts.Backend.LoadPath[0]
+	tmpl, err := template.New("p").Parse(p)
+	if err != nil {
+		return nil, err
+	}
+	buf := &bytes.Buffer{}
+	err = tmpl.Execute(buf, op)
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := getData(buf.String())
+	if err != nil {
+		return nil, err
+	}
+
+	resData, err := parseData(data, getFilePrefix(p))
+	if err != nil {
+		return nil, err
+	}
+	resources = resData
+	return resources, nil
+}
+
+func transferLangCode(lang string, lowerCase bool, cleanCode bool) string {
+	if lowerCase == true {
+		lang = toLowerCase(lang)
+	}
+	if cleanCode == true {
+		lang = toCleanCode(lang)
+	}
+	return lang
+}
+
+func getData(url string) ([]byte, error) {
+	if strings.Contains(url, "http://") || strings.Contains(url, "https://") {
+		resp, err := http.Get(url)
+		if err != nil {
+			return nil, err
+		}
+		defer func(Body io.ReadCloser) {
+			err := Body.Close()
+			if err != nil {
+
+			}
+		}(resp.Body)
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return nil, err
+		}
+		return body, nil
+	}
+	resp, err := os.ReadFile(url)
+	if err != nil {
+		return nil, err
+	}
+	return resp, nil
+
+}
+
+func parseData(data []byte, prefix string) (map[string]map[string]string, error) {
+	var res map[string]map[string]string
+	err := json.Unmarshal(data, &res)
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+}
+
+func getFilePrefix(fileName string) string {
+	all := path.Base(fileName)
+	suffix := path.Ext(fileName)
+	prefix := all[0 : len(all)-len(suffix)]
+	return prefix
+}
+
+func toCleanCode(code string) string {
+	ca := strings.Split(code, "-")
+	return ca[0]
+}
+
+func toLowerCase(code string) string {
+
+	return strings.ToLower(code)
+}
+
+func contains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
 }
--- i18n_test.go
+++ i18n_test.go
@@ -2,120 +2,29 @@ package i18next_test
 
 import (
 	"fmt"
-	"testing"
-
 	"github.com/yuangwei/go-i18next"
+	"testing"
 )
 
-func TestLngInLocal(t *testing.T) {
-	i18n := i18next.Init(i18next.I18nOptions{
-		Lng: []string{"en"},
-		Resources: map[string]map[string]string{
-			"en": {
-				"index.home": "Hello world",
-			},
-		},
-	})
-	text, err := i18n.T("index.home")
-	if err != nil {
-		t.Fatalf(`i18n.T("xxx") = %q,`, err)
-	}
-	fmt.Println(text)
-}
-
-func TestLngInFile(t *testing.T) {
-	i18n := i18next.Init(i18next.I18nOptions{
-		Lng: []string{"en"},
-		Backend: i18next.Backend{
-			LoadPath: []string{"/locale/{{.Lng}}/index.json"},
-		},
-	})
-	text, err := i18n.T("index.home")
-	if err != nil {
-		t.Fatalf(`i18n.T("index.home") = %q,`, err)
-	}
-	fmt.Println(text)
-}
-
-func TestLngInHttp(t *testing.T) {
-	i18n := i18next.Init(i18next.I18nOptions{
-		Lng: []string{"en"},
-		Backend: i18next.Backend{
-			LoadPath: []string{"https://yuangwei.com/example-data/go-i18next/locale/{{.Lng}}/index.json"},
-		},
-	})
-	text, err := i18n.T("index.home")
-	if err != nil {
-		t.Fatalf(`i18n.T("index.home") = %q,`, err)
-	}
-	fmt.Println(text)
-}
-
-func TestLngMutiFile(t *testing.T) {
-	i18n := i18next.Init(i18next.I18nOptions{
-		Lng: []string{"en"},
-		Backend: i18next.Backend{
-			LoadPath: []string{"/locale/{{.Lng}}/index.json", "/locale/{{.Lng}}/home.json"},
-		},
-	})
-	text, err := i18n.T("index.home")
-	if err != nil {
-		t.Fatalf(`i18n.T("index.home") = %q,`, err)
-	}
-	text, err = i18n.T("home.home")
-	if err != nil {
-		t.Fatalf(`i18n.T("home.home") = %q,`, err)
-	}
-	fmt.Println(text)
-}
-
-func TestChangeLng(t *testing.T) {
-	i18n := i18next.Init(i18next.I18nOptions{
-		Lng:        []string{"en", "jp"},
-		DefaultLng: "en",
-		Backend: i18next.Backend{
-			LoadPath: []string{"/locale/{{.Lng}}/index.json"},
-		},
-	})
-	_, err := i18n.T("index.home")
-	if err != nil {
-		t.Fatalf(`i18n.T("index.home") = %q,`, err)
-	}
-	err = i18n.ChangeLanguage("jp")
-	if err != nil {
-		t.Fatalf(`i18n.ChangeLanguage("jp") = %q`, err)
-	}
-	text, e := i18n.T("index.home")
-	if e != nil {
-		t.Fatalf(`i18n.T("index.home // jp") = %q,`, err)
-	}
-	fmt.Println(text)
-}
-
-func TestNs(t *testing.T) {
-	i18n := i18next.Init(i18next.I18nOptions{
-		Lng:       []string{"en"},
-		DefaultNs: "dev",
-		Backend: i18next.Backend{
-			LoadPath: []string{"/locale/{{.Ns}}/{{.Lng}}/index.json"},
-		},
-	})
-	_, err := i18n.T("index.home")
-	if err != nil {
-		t.Fatalf(`i18n.T("index.home") = %q,`, err)
-	}
-}
-
-func TestLoadAllInFile(t *testing.T) {
-	i18n := i18next.Init(i18next.I18nOptions{
+func TestI18nNext(t *testing.T) {
+	i18n, err := i18next.Init(i18next.I18nOptions{
 		Lng:        []string{"en", "cn"},
 		DefaultLng: "en",
 		Backend: i18next.Backend{
-			LoadPath: []string{"/locale/{{.Lng}}/*"},
+			LoadPath: []string{
+				"./examples/datas/json/{{.Lng}}/home.json",
+			},
 		},
 	})
-	_, err := i18n.T("index.home")
 	if err != nil {
-		t.Fatalf(`i18n.T("index.home") = %q,`, err)
+		t.Fatalf("i18n Initial error %s", err)
 	}
+	val, err := i18n.T("title", struct {
+		Name string
+	}{Name: "Mike"})
+
+	if err != nil {
+		t.Fatalf("i18n Format error %s", err)
+	}
+	fmt.Println(val)
 }
